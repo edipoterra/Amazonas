3f2f5d6f1aa049a3e6d3535cb960475b44c3d7be
edipoterra <edipoterra@gmail.com>
2011-06-18 20:13:49 -0300

    Inserindo funcionalidades de coordenadas

--- amazona.c
+++ amazona.c
@@ -5,45 +5,117 @@
 #include "pecas.h"
 
 char amazonas[10][10] = {
-        {' ',' ',' ','P',' ',' ','P',' ',' ',' '},
-        {' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
-        {' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
-        {'P',' ',' ',' ',' ',' ',' ',' ',' ','P'},
-        {' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
-        {' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
-        {'B',' ',' ',' ',' ',' ',' ',' ',' ','B'},
-        {' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
-        {' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
-        {' ',' ',' ','B',' ',' ','B',' ',' ',' '}
+	{' ',' ',' ','P',' ',' ','P',' ',' ',' '},
+		{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+		{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+		{'P',' ',' ',' ',' ',' ',' ',' ',' ','P'},
+		{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+		{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+		{'B',' ',' ',' ',' ',' ',' ',' ',' ','B'},
+		{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+		{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+		{' ',' ',' ','B',' ',' ','B',' ',' ',' '}
 };
 int vezJogador = 0;
-int 
+int fimJogo = 0;
 
 /* Troca jogador */
-int trocaJogador(){
-  if (vezJogador == 1) {
-    return 0;
-  }
-  else {
-    return 1;
-  }
+void trocaJogador(){
+	if (vezJogador == 1) {
+		vezJogador = 0;
+	}
+	else {
+		vezJogador = 1;
+	}
 }
 
+/* processo de validacao das coordenadas passadas por parametro */
+int validaCoordenadas(int num, char carac){
+	printf("%i\n",num);
+	printf("%c\n",carac);
+	if ((num < 1) || (num > 10)){
+		printf("\033[34;0f");
+		printf("Coordenada Invalida! X\n");
+		return 0; 
+	}
+	if ((carac != 'A') && (carac != 'a') &&
+		(carac != 'B') && (carac != 'b') &&
+		(carac != 'C') && (carac != 'c') &&
+		(carac != 'D') && (carac != 'd') &&
+		(carac != 'E') && (carac != 'e') &&
+		(carac != 'F') && (carac != 'f') &&
+		(carac != 'G') && (carac != 'g') &&
+		(carac != 'H') && (carac != 'h') &&
+		(carac != 'I') && (carac != 'i') &&
+		(carac != 'J') && (carac != 'j')){
+
+		printf("\033[34;0f");
+		printf("Coordenada Invalida! Y\n");
+		return 0;
+	}
+	return 1;
+}
 
 /* le posicoes iniciais de movimentacoes e finais para pecas */
+void leCoordPeca(){
+	char texto[3];
+	int numero;
+	char caracter;
+	printf("Entre com as coordenadas iniciais da peca: \n");
+	scanf("%s",texto);
+
+	if (texto[2] != ' '){
+		caracter = texto[2];
+		numero = (texto[0] - '0') * 10 + (texto[1] - '0');
+	}
+	else{
+		caracter = texto[1];
+		numero = texto[0] - '0';
+	}
+	if (validaCoordenadas(numero,caracter)){
+		printf("verdadeiro");
+	}
+	else {
+		printf("falso");
+	}
+
+	return;
+}
 
 /* le posicao final da flecha. Posicao inicial e a posicao final da peca */
+void leCoorFlecha(){
+	printf("Le coordenadas flecha");
+}
 
 /* valida jogadas verificando posicoes e blablabla */
+void validaMovimentacao(){
+	printf("Valida movimentacao");
+}
 
 /* processo que monta as mensagens para executar no jogo */
-
-
+void montaMensagens(){
+	if (vezJogador == 1){
+		printf("Pecas Brancas:\n");
+	}
+	else {
+		printf("Pecas Pretas:\n");
+	}
+	leCoordPeca();
+}
 
 int main(){
-  imprimeTabuleiro();
-  montaPecas(amazonas); 
+/*	system("clear");
+	imprimeTabuleiro();
+	montaPecas(amazonas); 
 
-  return 0;
+	while (!fimJogo){
+		montaMensagens();
+		trocaJogador();
+	} */
+	leCoordPeca();
+	leCoordPeca();
+	leCoordPeca();
+
+	return 0;
 }
 
